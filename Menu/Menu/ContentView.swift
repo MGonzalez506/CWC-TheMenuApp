4fab476530e1fc4ffbf6635e92fbdad5981beec4
mgonzalez506 <dev@mgonzalez506.com>
2024-04-04 23:13:45 -0600

    Subir código inicial

---
+++ Menu/Menu/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Menu
+//
+//  Created by Miguel González on 2024-04-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
